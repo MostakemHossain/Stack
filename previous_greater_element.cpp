59e110cc2e5ca69c6c9ed4ebc55a48fcf7b2f68e
MostakemHossain <mostakememon123@gmail.com>
2023-09-17 23:09:11 +0600

    prev greater & smaller element

---
+++ previous_greater_element.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> next_greater_elememt(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> output(n, -1);
+    stack<int> st; // index base
+    st.push(n - 1);
+    for (int i = n - 2; i >= 0; i--)
+    {
+        while (!st.empty() and arr[i] > arr[st.top()])
+        {
+            output[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    return output;
+}
+int main()
+{
+
+    // 10
+    // 4 6 3 1 0 9 5 6 7 3
+    // -1 -1 6 3 1 -1 9 9 9 7
+    int n;
+    cin >> n;
+    vector<int> v;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    vector<int> result = next_greater_elememt(v);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << '\n';
+}
